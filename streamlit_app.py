21573a379b7656bdeeb1f5a90c09395b03acfca1
haripriya pinthepu <pinthepu.haripriya@gmail.com>
2022-11-01 10:00:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
-streamlit.header('breakfast meanu')
+streamlit.header('breakfast menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('hard boiled egg')
