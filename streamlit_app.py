ccf638ed2c50bc579d04e411620afa6f7da71009
haripriya pinthepu <pinthepu.haripriya@gmail.com>
2022-11-02 19:19:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("the fruit_load_list contains")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
